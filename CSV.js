92f447d555e64527c5f0435ffed63b9b9a48a54a
Ervin <68126398+ecent16@users.noreply.github.com>
2024-07-19 12:53:55 -0400

    Update CSV.js

--- CSV.js
+++ CSV.js
@@ -1,1 +1,15 @@
 console.log('This is a test');
+const loadCSVFile = (fileName) => {
+  
+  const fileExtension = fileName.split('.')[1].toLowerCase();
+  if(fileExtension !== 'csv') {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const txt = e.target.result;
+      document.write(txt);
+    }
+    reader.readAsText(fileName);
+  }
+  else { return false; }
+}
+
