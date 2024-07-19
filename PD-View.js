1d46fcd1c12f226259e754566c2d7381e4dabb98
Ervin <68126398+ecent16@users.noreply.github.com>
2024-07-19 13:44:58 -0400

    Update and rename ViewCSVFileData.js to PD-View.js

---
+++ PD-View.js
@@ -0,0 +1,14 @@
+
+const populateTable = (csvData) => {
+  // 
+  const table = document.getElementById('mainTable');
+  const fileInput = document.getElementById('fileInput');
+
+  fileInput.addEventListener('change', () => {
+    if(fileInput !== null || fileInput.files.length > 0) {
+      
+      
+    }
+    else { return; }
+  });
+}
